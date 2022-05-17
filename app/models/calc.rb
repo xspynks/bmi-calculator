8245185d5e945d4d679024daf75b2a0664538238
Rafael de Paula <xspynks@gmail.com>
2022-05-17 04:45:41 -0300

    refact: adiciona live update da tabela de calculo ao adicionar um novo

--- app/models/calc.rb
+++ app/models/calc.rb
@@ -1,5 +1,5 @@
 class Calc < ApplicationRecord
 
-  after_create_commit -> { broadcast_append_to "calcs", partial: "calcs/calc", locals: { calc: self }, target: "calcs" }
+  after_create_commit -> { broadcast_prepend_to "calcs", partial: "calcs/calc", locals: { calc: self }, target: "calc_stream" }
 
 end
