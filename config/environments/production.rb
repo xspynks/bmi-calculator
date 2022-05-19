14d37048b20f30ca02e9326e86198d585ca776b7
Rafael de Paula <xspynks@gmail.com>
2022-05-18 23:57:37 -0300

    refact: configura Redis (5)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,10 +41,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-   config.action_cable.disable_request_forgery_protection = true
-   config.action_cable.url = "ws://localhost:3000/cable"
-   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
-   config.action_cable.allowed_request_origins = /(\.dev$)|^localhost$/
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = "wss://example.com/cable"
+  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
